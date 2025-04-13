b6367b3868e14b1914d5b98a8bb68a0c76932e90
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-04-13 20:14:57 -0300

    feat: update theme switcher

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -15,13 +15,13 @@ export function ThemeSwitcher() {
       whileTap={{ scale: 0.95 }}
       onClick={toggleTheme}
       className="relative inline-flex items-center justify-center w-10 h-10 text-sm font-medium 
-                rounded-md border-2 border-black dark:border-white bg-accent text-white
-                shadow-custom dark:shadow-custom-white hover:shadow-none hover:translate-x-1 
+                rounded-md border-2 border-border bg-accent
+                shadow-custom hover:shadow-none hover:translate-x-1 
                 hover:translate-y-1 transition-all"
       aria-label="Alternar tema"
     >
-      <Sun className="h-5 w-5 hidden dark:inline" />
-      <Moon className="h-5 w-5 inline dark:hidden" />
+      <Sun className="h-5 w-5 text-accent-foreground dark:hidden" />
+      <Moon className="h-5 w-5 hidden text-accent-foreground dark:inline" />
       <span className="sr-only">Alternar tema</span>
     </motion.button>
   );
