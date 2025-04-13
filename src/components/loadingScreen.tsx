3f7c979114fc34588a7810b78070db8f3c63ac7e
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-04-13 19:19:44 -0300

    fix: improve loading screen component with consistent styling

--- src/components/loadingScreen.tsx
+++ src/components/loadingScreen.tsx
@@ -7,7 +7,9 @@ const LoadingScreen: React.FC = () => {
       <div className="relative">
         {/* Main loading container */}
         <motion.div
-          className="text-5xl sm:text-6xl font-black bg-white dark:bg-background p-8 border-4 border-black dark:border-white shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] dark:shadow-[8px_8px_0px_0px_rgba(255,255,255,1)]"
+          className="text-5xl sm:text-6xl font-black bg-white dark:bg-background p-8 
+                    border-4 border-black dark:border-white 
+                    shadow-custom-lg dark:shadow-custom-lg-white"
           initial={{ rotate: 5 }}
           animate={{ rotate: [5, -3, 5] }}
           transition={{ duration: 5, repeat: Infinity, ease: "easeInOut" }}
@@ -34,14 +36,16 @@ const LoadingScreen: React.FC = () => {
 
         {/* Decorative elements */}
         <motion.div
-          className="absolute -top-10 -right-10 w-20 h-20 bg-accent border-4 border-black dark:border-white shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] rounded-full"
+          className="absolute -top-10 -right-10 w-20 h-20 bg-accent border-4 border-black 
+                   dark:border-white shadow-custom dark:shadow-custom-white rounded-full"
           initial={{ scale: 0 }}
           animate={{ scale: 1, rotate: 360 }}
           transition={{ duration: 1, delay: 0.2 }}
         />
 
         <motion.div
-          className="absolute -bottom-8 -left-8 w-16 h-16 bg-green-400 border-4 border-black dark:border-white shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] rounded-md"
+          className="absolute -bottom-8 -left-8 w-16 h-16 bg-green-400 border-4 border-black 
+                   dark:border-white shadow-custom dark:shadow-custom-white rounded-md"
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ duration: 0.5, delay: 0.4 }}
@@ -49,7 +53,8 @@ const LoadingScreen: React.FC = () => {
 
         {/* Rotating square */}
         <motion.div
-          className="absolute -bottom-12 -right-12 w-24 h-24 bg-blue-400 border-4 border-black dark:border-white shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] dark:shadow-[6px_6px_0px_0px_rgba(255,255,255,1)]"
+          className="absolute -bottom-12 -right-12 w-24 h-24 bg-blue-400 border-4 border-black 
+                   dark:border-white shadow-custom dark:shadow-custom-white"
           initial={{ scale: 0, rotate: 0 }}
           animate={{ scale: 1, rotate: [0, 180, 360] }}
           transition={{ duration: 3, repeat: Infinity, delay: 0.6, ease: "easeInOut" }}
